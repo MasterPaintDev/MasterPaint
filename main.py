ef8d5183fcc19dfe7af75f21ace9685c084de608
MasterpaintSu <159675013+MasterpaintSu@users.noreply.github.com>
2024-02-10 22:16:03 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,28 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QPushButton, QWidget
 from drawing_widget import DrawingWidget
 
-
-class MainWindow(QMainWindow):
+class PaintApp(QMainWindow):
     def __init__(self):
         super().__init__()
 
-        self.setWindowTitle("Drawing App")
+        self.setWindowTitle("Paint App")
         self.setGeometry(100, 100, 800, 600)
 
-        self.drawing_widget = DrawingWidget()
-        self.setCentralWidget(self.drawing_widget)
+        self.canvas = DrawingWidget()
+        self.clear_button = QPushButton("Clear Canvas")
+        self.clear_button.clicked.connect(self.canvas.clear_canvas)  # Conecta al método clear_canvas
 
+        layout = QVBoxLayout()
+        layout.addWidget(self.canvas)
+        layout.addWidget(self.clear_button)
+
+        central_widget = QWidget()
+        central_widget.setLayout(layout)
+        self.setCentralWidget(central_widget)
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    window = MainWindow()
+    window = PaintApp()
     window.show()
     sys.exit(app.exec_())
