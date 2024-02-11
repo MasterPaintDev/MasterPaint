665f7becc09f6e0328c30221164d84a964cb119f
staFF6773 <108166164+staFF6773@users.noreply.github.com>
2024-02-11 17:46:57 -0600

    Update test.c

--- test/test.c
+++ test/test.c
@@ -1,7 +1,22 @@
 #include <stdio.h>
 
 int main() {
-    // Imprimir "¡Hola, mundo!" en la pantalla
-    printf("¡Hola, mundo!\n");
+    // Declarar variables para almacenar los números ingresados por el usuario
+    int numero1, numero2;
+    
+    // Pedir al usuario que ingrese el primer número
+    printf("Ingrese el primer número: ");
+    scanf("%d", &numero1);
+    
+    // Pedir al usuario que ingrese el segundo número
+    printf("Ingrese el segundo número: ");
+    scanf("%d", &numero2);
+    
+    // Calcular la suma de los dos números
+    int suma = numero1 + numero2;
+    
+    // Mostrar el resultado al usuario
+    printf("La suma de %d y %d es: %d\n", numero1, numero2, suma);
+    
     return 0;
 }
