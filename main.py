23a897a17065e71c4df95ec2b2e52a360159b420
staFF6773 <108166164+staFF6773@users.noreply.github.com>
2024-02-10 22:52:32 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QAction, QMenu, QVBoxLayout, QHBoxLayout, QPushButton, QWidget, QLabel, QColorDialog, QFileDialog, QMessageBox
+from PyQt5.QtWidgets import QApplication, QMainWindow, QAction, QMenu, QVBoxLayout, QHBoxLayout, QPushButton, QWidget, QLabel, QColorDialog, QFileDialog, QMessageBox, QTextEdit, QTabWidget
 from drawing_widget import DrawingWidget
 
 class PaintApp(QMainWindow):
@@ -15,6 +15,7 @@ class PaintApp(QMainWindow):
         self.create_actions()
         self.create_menus()
         self.create_toolbars()
+        self.create_updates_tab()
 
     def create_actions(self):
         self.open_action = QAction("Open", self)
@@ -32,15 +33,17 @@ class PaintApp(QMainWindow):
         self.exit_action = QAction("Exit", self)
         self.exit_action.triggered.connect(self.confirm_exit)
 
+        self.show_updates_action = QAction("Show Updates", self)
+        self.show_updates_action.triggered.connect(self.show_updates)
+
     def create_menus(self):
         file_menu = self.menuBar().addMenu("File")
         file_menu.addAction(self.open_action)
         file_menu.addAction(self.save_action)
         file_menu.addAction(self.exit_action)
 
-        edit_menu = self.menuBar().addMenu("Edit")
-        edit_menu.addAction(self.clear_action)
-        edit_menu.addAction(self.choose_color_action)
+        updates_menu = self.menuBar().addMenu("Updates")
+        updates_menu.addAction(self.show_updates_action)
 
     def create_toolbars(self):
         toolbar = self.addToolBar("Tools")
@@ -56,6 +59,23 @@ class PaintApp(QMainWindow):
         color_button = QPushButton("Choose Color")
         color_button.clicked.connect(self.choose_color)
         toolbar.addWidget(color_button)
+
+    def create_updates_tab(self):
+        self.updates_tab = QWidget()
+        self.updates_layout = QVBoxLayout()
+        self.updates_tab.setLayout(self.updates_layout)
+
+        self.updates_text = QTextEdit()
+        self.updates_text.setReadOnly(True)
+        self.updates_layout.addWidget(self.updates_text)
+
+        self.tab_widget = QTabWidget()  # Aquí está la corrección
+        self.tab_widget.addTab(self.canvas, "Canvas")
+        self.tab_widget.addTab(self.updates_tab, "Updates")
+
+        self.setCentralWidget(self.tab_widget)  # Establecer la pestaña como widget central
+
+        self.updates_text.setPlainText("Version 1.0:\n- Initial release\n\nVersion 1.1:\n- Added Updates tab\n- Bug fixes")
 
     def choose_color(self):
         color = QColorDialog.getColor()
@@ -78,6 +98,9 @@ class PaintApp(QMainWindow):
 
         if reply == QMessageBox.Yes:
             self.close()
+
+    def show_updates(self):
+        self.tab_widget.setCurrentIndex(1)  # Cambiar a la pestaña de actualizaciones
 
     def closeEvent(self, event):
         if self.canvas.isModified():
